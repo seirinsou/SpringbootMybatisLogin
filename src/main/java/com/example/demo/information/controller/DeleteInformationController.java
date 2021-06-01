7e6c45986835d74ced79d9490dc7b95db255732e
seirinsou <scl0905myc@gmail.com>
2021-06-01 20:01:34 +0900

    0601final

--- src/main/java/com/example/demo/information/controller/DeleteInformationController.java
+++ src/main/java/com/example/demo/information/controller/DeleteInformationController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.information.model.DeleteInformationInDTO;
 import com.example.demo.information.model.DeleteInformationOutDTO;
 import com.example.demo.information.service.DeleteInformationService;
 
@@ -15,16 +17,28 @@ public class DeleteInformationController {
     DeleteInformationService deleteInformationService;
     
     @RequestMapping("delete")
-    public String toDeleteCheck(Model model,@ModelAttribute DeleteInformationOutDTO delInfor){
-        DeleteInformationOutDTO delInfor1 = deleteInformationService.findByIdx(delInfor.getIdx());
-        model.addAttribute("delInfor", delInfor);
+    public String toDeleteCheck(Model model,@ModelAttribute DeleteInformationOutDTO delInforOut, DeleteInformationInDTO delInforIn){
+        DeleteInformationOutDTO delInforOutDTO = deleteInformationService.findByIdx(delInforOut.getIdx());
+        delInforIn.setIdx(delInforOutDTO.getIdx());
+        delInforIn.setName(delInforOutDTO.getName());
+        delInforIn.setSex(delInforOutDTO.getSex());
+        delInforIn.setPhoneNumber(delInforOutDTO.getPhoneNumber());
+        delInforIn.setApplicationDate(delInforOutDTO.getApplicationDate());
+        delInforIn.setBornDate(delInforOutDTO.getBornDate());
+        delInforIn.setAddress(delInforOutDTO.getAddress());
+        delInforIn.setManager(delInforOutDTO.getManager());
+        delInforIn.setNetPlan(delInforOutDTO.getNetPlan());
+        delInforIn.setNetType(delInforOutDTO.getNetType());
+        delInforIn.setNotes(delInforOutDTO.getNotes());
+        delInforIn.setPostcode(delInforOutDTO.getPostcode());
+        model.addAttribute("delInforOut", delInforIn);
         return "deleteCheck";
     }
     
     @RequestMapping("/deleinfor")
-    public String deleteInformation(Model model,@ModelAttribute DeleteInformationOutDTO delInfor, Integer idx) {
+    public String deleteInformation(Model model,@ModelAttribute DeleteInformationInDTO delInforIn, Integer idx) {
         deleteInformationService.delete(idx);
-        model.addAttribute("user",delInfor);
+        model.addAttribute("user",delInforIn);
         return "redirect:/search";
     }
 
